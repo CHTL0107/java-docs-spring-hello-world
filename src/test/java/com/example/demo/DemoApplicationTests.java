09216f9b49715410e8e52ba3306e7fb82833d80c
CHTL0107 <117682565+CHTL0107@users.noreply.github.com>
2022-11-07 21:52:20 +0100

    add

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -9,5 +9,9 @@ class DemoApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
+@Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(image, 0, 0, this); // see javadoc for more info on the parameters            
+    }
 }
