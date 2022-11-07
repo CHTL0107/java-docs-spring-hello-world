6212584b66e6e648e939f99d0b7d0126068d9190
CHTL0107 <117682565+CHTL0107@users.noreply.github.com>
2022-11-07 22:12:40 +0100

    clear all

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,14 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.awt.Graphics;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.imageio.ImageIO;
-import javax.swing.JPanel;
+
 @SpringBootApplication
 @RestController
 public class DemoApplication {
@@ -22,11 +15,6 @@ public class DemoApplication {
 
 	@RequestMapping("/")
 	String sayHello() {
-		return "<button> btn </button> Hello to Chayma's Empire From Azure!";
-
-
-	}
-    
-
+		return "Hello World!";
 	}
 }
