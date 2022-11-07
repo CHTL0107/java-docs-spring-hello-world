ee6ee158b44a9901e1f6e3a8bc529e35734585ea
CHTL0107 <117682565+CHTL0107@users.noreply.github.com>
2022-11-07 21:46:38 +0100

    add image

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,7 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
 @SpringBootApplication
 @RestController
 public class DemoApplication {
@@ -16,5 +23,26 @@ public class DemoApplication {
 	@RequestMapping("/")
 	String sayHello() {
 		return "Hello to Chayma's Empire From Azure!";
+
 	}
+
+	public class ImagePanel extends JPanel{
+
+    private BufferedImage image;
+
+    public ImagePanel() {
+       try {                
+          image = ImageIO.read(new File("https://img.freepik.com/photos-gratuite/rose-rose-rose-petale_1339-1165.jpg?w=900&t=st=1667853926~exp=1667854526~hmac=ede553eb261fdd5530e6c3bff23177a151ced6242aec3306482999e4833f13ea"));
+       } catch (IOException ex) {
+            // handle exception...
+       }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(image, 0, 0, this); // see javadoc for more info on the parameters            
+    }
+
+}
 }
